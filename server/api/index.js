80a3d44b5c868224eb6eeb07de7ccc6b539477d6
rodrigojm <rodrigomallmann1@gmail.com>
2020-05-06 14:16:12 -0400

    added agent

--- server/api/index.js
+++ server/api/index.js
@@ -10,6 +10,27 @@ const generateCode = function() {
   )
 }
 
+const Socks = require('socks')
+
+const fixieUrl = process.env.FIXIE_SOCKS_HOST
+const fixieValues = fixieUrl.split(new RegExp('[/(:\\/@)/]+'))
+
+const socksAgent = new Socks.Agent(
+  {
+    proxy: {
+      ipaddress: fixieValues[2],
+      port: fixieValues[3],
+      type: 5,
+      authentication: {
+        username: fixieValues[0],
+        password: fixieValues[1]
+      }
+    }
+  },
+  true, // true HTTPS server, false for HTTP server
+  false // rejectUnauthorized option passed to tls.connect()
+)
+
 router.get('/search/:value', async (req, res, next) => {
   try {
     const options = {
@@ -24,8 +45,9 @@ router.get('/search/:value', async (req, res, next) => {
       headers: {
         'Content-Type': 'application/json',
         Authorization:
-          'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1ODg1OTg4MzgsImV4cCI6MTU4ODY4NTIzOCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI5ZDljMTRlNTU1NzM0YjMzYmJmMmY1ZWU2YmVmMjg2ZSIsInNjb3BlIjpbImJhc2ljIl19.Gx8wLfSNlStlHBv9skG5MQqzLzSqfDIG7Z4O7U-lWeZqv5jxI6fRzGv9GiVq3S77vHq7ru3IxLQbMShRSWt2Pj9gSbTOKIhGdGTvidnXaCVtTjPj2bDNH-BbboKgKmxLB57u0l5DFrGuUb8Yv7vC0HBaQ0W_3iCDywBzc7sjzCB8WCwVc_DzUwxgij--qi1lKx2C6GJoSqu0Ryf2btaq2SKdunGYW_ko3GR6bj2qIJPYA9DPG84czGjxNi7Qb1NLKhA86WxE-98x1DHmh259djxm8rPSUzffkoRykxjr07mTSa8F2EeuolaOF6wYBabsn1qrgsmUNcvjdBPDmhtuqQ'
-      }
+          'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1ODg3ODg3NzgsImV4cCI6MTU4ODg3NTE3OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI5ZDljMTRlNTU1NzM0YjMzYmJmMmY1ZWU2YmVmMjg2ZSIsInNjb3BlIjpbImJhc2ljIl19.EvGAP5yfPScCesGeCgpTg6lQv813o4vIW-mfKMViSFseYt9XHmNFvj2U99gGnjmnXUkWYnGKimTqLdrxxwnDUjHPyC9vWolgmQ_yU_PtXTaUzEKxxYLwgXZ_w3n3lkOxdnL2GD46p4ZtFXexisH97XydtapFbcu2RB_QUHXymN952jgz5Li7oKqvJgfmr6WPn4TpSNzHD9B5XwUfvAt_x101wcLgC1Gkb3XB4MOKm4lUe5twN-I5IJhvfDRblvN_lCxU8KCi-fJz7hy2_tcPN449mKVfFrKQoX7jB4XyeTLm7pN4DnNoIM6QeJ5tUVvBg10sfEM7NTcUh6YtCI8hVg'
+      },
+      agent: socksAgent
     }
     requestPromise(options).then(response => {
       res.json(response)
@@ -48,8 +70,9 @@ router.get('/select/:itemId', async (req, res, next) => {
       headers: {
         'Content-Type': 'application/json',
         Authorization:
-          'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1ODg1OTg4MzgsImV4cCI6MTU4ODY4NTIzOCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI5ZDljMTRlNTU1NzM0YjMzYmJmMmY1ZWU2YmVmMjg2ZSIsInNjb3BlIjpbImJhc2ljIl19.Gx8wLfSNlStlHBv9skG5MQqzLzSqfDIG7Z4O7U-lWeZqv5jxI6fRzGv9GiVq3S77vHq7ru3IxLQbMShRSWt2Pj9gSbTOKIhGdGTvidnXaCVtTjPj2bDNH-BbboKgKmxLB57u0l5DFrGuUb8Yv7vC0HBaQ0W_3iCDywBzc7sjzCB8WCwVc_DzUwxgij--qi1lKx2C6GJoSqu0Ryf2btaq2SKdunGYW_ko3GR6bj2qIJPYA9DPG84czGjxNi7Qb1NLKhA86WxE-98x1DHmh259djxm8rPSUzffkoRykxjr07mTSa8F2EeuolaOF6wYBabsn1qrgsmUNcvjdBPDmhtuqQ'
-      }
+          'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1ODg3ODg3NzgsImV4cCI6MTU4ODg3NTE3OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI5ZDljMTRlNTU1NzM0YjMzYmJmMmY1ZWU2YmVmMjg2ZSIsInNjb3BlIjpbImJhc2ljIl19.EvGAP5yfPScCesGeCgpTg6lQv813o4vIW-mfKMViSFseYt9XHmNFvj2U99gGnjmnXUkWYnGKimTqLdrxxwnDUjHPyC9vWolgmQ_yU_PtXTaUzEKxxYLwgXZ_w3n3lkOxdnL2GD46p4ZtFXexisH97XydtapFbcu2RB_QUHXymN952jgz5Li7oKqvJgfmr6WPn4TpSNzHD9B5XwUfvAt_x101wcLgC1Gkb3XB4MOKm4lUe5twN-I5IJhvfDRblvN_lCxU8KCi-fJz7hy2_tcPN449mKVfFrKQoX7jB4XyeTLm7pN4DnNoIM6QeJ5tUVvBg10sfEM7NTcUh6YtCI8hVg'
+      },
+      agent: socksAgent
     }
     requestPromise(options).then(response => {
       console.log(response)
